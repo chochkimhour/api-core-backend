2b28bbaa69bd30f0691cc1084ea33e7e2766f1c4
Choch Kimhour <kimhour.choch@ecoinsoft.com>
2026-04-27 16:00:39 +0700

    Clean code

--- src/swagger/express.ts
+++ src/swagger/express.ts
@@ -19,9 +19,9 @@ async function loadSwaggerUiExpress(): Promise<SwaggerUiExpress> {
     const module = await dynamicImport("swagger-ui-express");
     return (module.default ?? module) as SwaggerUiExpress;
   } catch (error) {
+    void error;
     throw new Error(
       "setupSwaggerDocs() requires the optional peer dependency swagger-ui-express. Install it with: npm install swagger-ui-express",
-      { cause: error },
     );
   }
 }
